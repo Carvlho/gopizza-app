4818216e7bedf07033eb584003bcced3d35a9022
Igor Carvalho <36631966+Carvlho@users.noreply.github.com>
2022-05-04 10:48:30 -0300

    Update
    added signIn

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+const SignIn = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
+
+export default SignIn;
