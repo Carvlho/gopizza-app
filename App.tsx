daa5fd8a369ffd91f861bf4b186b337d32875a12
IgorCarvlho <101196469+IgorCarvlho@users.noreply.github.com>
2022-05-13 14:54:58 -0300

    Added Status Bar

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 import { ThemeProvider } from "styled-components/native";
 import AppLoading from "expo-app-loading";
-import theme  from "@src/theme";
+import theme from "./src/theme";
 
-import SignIn from "@src/screens/SignIn";
+import SignIn from "@screens/SignIn";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,6 +20,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
       <SignIn />
     </ThemeProvider>
   );
