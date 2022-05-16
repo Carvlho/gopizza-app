e99e864f6a69eecbbbb9a52861de8ac64ca72b8b
IgorCarvlho <101196469+IgorCarvlho@users.noreply.github.com>
2022-05-16 10:49:50 -0300

    added the authentication hook

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 import { ThemeProvider } from "styled-components/native";
-import AppLoading from "expo-app-loading";
-import theme from "./src/theme";
+import { AuthProvider } from "./src/hooks/auth";
 
 import SignIn from "@screens/SignIn";
-import { StatusBar } from "expo-status-bar";
+
+import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +23,9 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" translucent backgroundColor="transparent" />
-      <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
